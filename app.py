2b04f6e772ddccc3f67e6588e1867a11897ed4b9
Rory Sweeney <r.sweeney4@universityofgalway.ie>
2023-11-15 11:33:58 +0000

    Changes listed below: 1. Removed function get_tune_given_name2. 2. Modified the function dl_tune_names() to also download tune IDs. 3. Changed the searchType value used to indicate a search for a tune title from 'metadata' to 'title'. 4. Changed the 'query' parameter name to 'searchTerm'. 5. Added the function getPatterns() to download the most common patterns in a tune for display on the composition page. 6. Added the function getNeighbourPatterns() to request the pattern nodes for the network graph visualisation. 7. Added the function getNeighbourTunes() to request the tune nodes for the network graph visualisation. 8. Added the function getTuneData() to request tune metadata for display on the composition page. 9. Added the function getTuneFamilyMembers() to request the list of tunes contained in a given tune family for display on the tune family page. 10. Incuded tuneID in the returned variables of get_pattern_search_query() to allow for routing to the relevant composition page, which is referenced by tune ID. 11. Modified the get_most_common_patterns_for_a_tune() function such that the query generated searches by tune ID rather than tune title, and to include the excludeTrivialPatterns variable which is used to turn on and off filtering by pattern complexity. 12. Modified the get_tune_given_name() function to generate a query to perfom a fuzzy search for tune titles by matching tune IDs instead of tune title strings. 13. Modified the advanced_search() function to generate a query to perform fuzzy searches using matched tune IDs as above and also to perform searches for any permutation of empty or non-empty search fields. 14. Modified the get_all_tune_names() function to generate a query to also return tune IDs. 15. Added the get_neighbour_patterns_by_tune() function to generate a query to return some common patterns belonging to a tune for the network graph visualisation. 16. Added the get_neighbour_tunes_by_pattern() function to generate a query to return some tunes containing a given pattern for the network graph visualisation. 17. Added the get_tune_data() function to generate a query to return a tune's title and tune family for display on the composition page. 18. Added the get_tune_family_members() function to generate a query to return the list of tunes in a tune family for display on the tune family page.

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
 from query_factory import (get_tune_given_name, get_all_tune_names,
-                           get_tune_given_name2, get_pattern_search_query,
-                           advanced_search)
+                           get_pattern_search_query,
+                           advanced_search, get_most_common_patterns_for_a_tune,
+                           get_neighbour_patterns_by_tune, get_neighbour_tunes_by_pattern,
+                           get_tune_data, get_tune_family_members)
 
 app = Flask(__name__)
 CORS(app)
@@ -33,13 +35,19 @@ def dl_tune_names():
             'format': 'json'
         }
     )
+    #print(sparql_query)
+    #print(response.text)
     # Check the response status
     if response.status_code != 200:
         print('Failed to execute SPARQL query')
         #TODO: Correctly handle this error.
     else:
         namesJSON = response.json()
-        all_names = [item['tune_name']['value'] for item in namesJSON['results']['bindings']]
+        #all_names = [item['title']['value'] for item in namesJSON['results']['bindings']]
+        all_names = [[item[var]['value'] for item in namesJSON['results']['bindings']] for var in ['title', 'id']]
+        #print(all_names)
+        #print(all_names[0][200])
+        #print(all_names[1][200])
 
 def mock_data(query_params):
     # Mock data
@@ -101,18 +109,18 @@ def search():
     query_params = request.args.to_dict()
     if MOCK:
         return mock_data(query_params)
-    if(query_params['searchType'] == "metadata"):
+    if(query_params['searchType'] == "title"):
         # Generate the SPARQL query
-        sparql_query = get_tune_given_name(query_params['query'], all_names)
+        sparql_query = get_tune_given_name(query_params['searchTerm'], all_names)
     elif(query_params['searchType'] == "pattern"):
-        sparql_query = get_pattern_search_query(query_params['query'])
+        sparql_query = get_pattern_search_query(query_params['searchTerm'])
         #print(sparql_query)
     elif(query_params['searchType'] == "advanced"):
-        print(query_params)
+        #print(query_params)
         sparql_query = advanced_search(query_params, all_names)
     else:
-        # Generate the SPARQL query
-        sparql_query = get_tune_given_name2(query_params['query'])
+        # Error message.
+        return jsonify({'error': 'Invalid search type.'}), 501
     # Execute the SPARQL query
     response = requests.post(
         BLAZEGRAPH_URL,
@@ -134,6 +142,128 @@ def search():
 def similarity_measures_api():
     # Return a list of similarity measures
     return jsonify(["Measure 1", "Measure 2", "Measure 3"]), 200
+
+
+@app.route('/api/patterns', methods=['GET'])
+def getPatterns():
+    # Get the query parameters from the GET request
+    query_params = request.args.to_dict()
+    # Generate the SPARQL query
+    #print(query_params)
+    sparql_query = get_most_common_patterns_for_a_tune(query_params['id'],
+                                                       query_params['excludeTrivialPatterns'])
+    # Execute the SPARQL query
+    response = requests.post(
+        BLAZEGRAPH_URL,
+        data={
+            'query': sparql_query,
+            'format': 'json'
+        }
+    )
+    #print(sparql_query)
+    #print(response.text)
+    # Check the response status
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to execute SPARQL query'}), 500
+    # Return the JSON data
+    return jsonify(response.json()), 200
+
+
+@app.route('/api/neighbour_patterns', methods=['GET'])
+def getNeighbourPatterns():
+    # Get the query parameters from the GET request
+    query_params = request.args.to_dict()
+    # Generate the SPARQL query
+    #print(query_params)
+    sparql_query = get_neighbour_patterns_by_tune(query_params['id'],
+                                                  query_params['excludeTrivialPatterns'])
+    # Execute the SPARQL query
+    response = requests.post(
+        BLAZEGRAPH_URL,
+        data={
+            'query': sparql_query,
+            'format': 'json'
+        }
+    )
+    #print(sparql_query)
+    #print(response.text)
+    # Check the response status
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to execute SPARQL query'}), 500
+    # Return the JSON data
+    return jsonify(response.json()), 200
+
+
+@app.route('/api/neighbour_tunes', methods=['GET'])
+def getNeighbourTunes():
+    # Get the query parameters from the GET request
+    query_params = request.args.to_dict()
+    # Generate the SPARQL query
+    #print(query_params)
+    sparql_query = get_neighbour_tunes_by_pattern(query_params['id'])
+    # Execute the SPARQL query
+    response = requests.post(
+        BLAZEGRAPH_URL,
+        data={
+            'query': sparql_query,
+            'format': 'json'
+        }
+    )
+    #print(sparql_query)
+    #print(response.text)
+    # Check the response status
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to execute SPARQL query'}), 500
+    # Return the JSON data
+    return jsonify(response.json()), 200
+
+
+@app.route('/api/tune_by_id', methods=['GET'])
+def getTuneData():
+    # Get the query parameters from the GET request
+    query_params = request.args.to_dict()
+    # Generate the SPARQL query
+    #print(query_params)
+    sparql_query = get_tune_data(query_params['id'])
+    # Execute the SPARQL query
+    response = requests.post(
+        BLAZEGRAPH_URL,
+        data={
+            'query': sparql_query,
+            'format': 'json'
+        }
+    )
+    #print(sparql_query)
+    #print(response.text)
+    # Check the response status
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to execute SPARQL query'}), 500
+    # Return the JSON data
+    return jsonify(response.json()), 200
+
+
+@app.route('/api/tuneFamilyMembers', methods=['GET'])
+def getTuneFamilyMembers():
+    # Get the query parameters from the GET request
+    query_params = request.args.to_dict()
+    # Generate the SPARQL query
+    #print(query_params)
+    sparql_query = get_tune_family_members(query_params['family'])
+    # Execute the SPARQL query
+    response = requests.post(
+        BLAZEGRAPH_URL,
+        data={
+            'query': sparql_query,
+            'format': 'json'
+        }
+    )
+    print(sparql_query)
+    print(response.text)
+    # Check the response status
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to execute SPARQL query'}), 500
+    # Return the JSON data
+    return jsonify(response.json()), 200
 
 
 app.setup = [(None, dl_tune_names())]
--- query_factory.py
+++ query_factory.py
@@ -9,12 +9,13 @@ def get_pattern_search_query(pattern):
     sparql_query =   """PREFIX jams:<http://w3id.org/polifonia/ontology/jams/>
                         PREFIX mm:<http://w3id.org/polifonia/ontology/music-meta/>
                         PREFIX ptn:<http://w3id.org/polifonia/resource/pattern/>
-                        SELECT distinct ?tune_name ?tuneType ?key ?signature
+                        SELECT distinct ?tune_name ?tuneType ?key ?signature ?id
                         WHERE
                         {
 	                        ?obs jams:ofPattern ptn:""" + pattern + """.
                             ?annotation jams:includesObservation ?obs.
                             ?annotation jams:isJAMSAnnotationOf ?musicalComposition.
+                            ?musicalComposition jams:tuneId ?id.
                             OPTIONAL {?musicalComposition mm:title ?tune_name}
                             OPTIONAL {?musicalComposition jams:tuneType ?tuneType}
                             OPTIONAL {?musicalComposition jams:key ?key}
@@ -23,62 +24,58 @@ def get_pattern_search_query(pattern):
     return sparql_query
 
 
-def get_most_common_patterns_for_a_tune(tune):
+def get_most_common_patterns_for_a_tune(id, excludeTrivialPatterns):
     sparql_query =   """PREFIX jams:<http://w3id.org/polifonia/ontology/jams/>
                         PREFIX mm: <http://w3id.org/polifonia/ontology/music-meta/>
-                        SELECT ?tune_name ?pattern (count(?pattern) as ?patternFreq)
+                        SELECT ?pattern (count(?pattern) as ?patternFreq)
                         {
-                            ?tune_file mm:title ?tune_name.
-                            FILTER (?tune_name = \"""" + tune + """\").
-                            ?xx jams:isJAMSAnnotationOf ?tune_file.
+                            ?tune jams:tuneId \"""" + id + """\".
+                            ?xx jams:isJAMSAnnotationOf ?tune.
                             ?xx jams:includesObservation ?observation .
-                            ?observation jams:ofPattern ?pattern .
-                        } group by ?tune_name ?pattern
-                        order by DESC (?patternFreq )"""
+                        """
+    if excludeTrivialPatterns == "true":
+        sparql_query += """ ?observation jams:hasPatternComplexity ?comp .
+                            FILTER (?comp > "0.4"^^xsd:float) .
+                        """
+    sparql_query +=     """?observation jams:ofPattern ?pattern .
+                        } group by ?pattern
+                        order by DESC (?patternFreq ) LIMIT 10"""
     return sparql_query
 
 
 # Fuzzy Search
 def get_tune_given_name(query_name, all_names):
-    matched_tuples = process.extractBests(query_name, all_names, score_cutoff=60, limit=100)
-    print(matched_tuples)
-    matched_names = [x[0] for x in matched_tuples]
+    names_dict = {i: val for i, val in enumerate(all_names[0])}
+    matched_tuples = process.extractBests(query_name, names_dict, score_cutoff=60)
+    #print(matched_tuples)
+    matched_name_indices = [x[2] for x in matched_tuples]
+    #print(matched_name_indices)
+    matched_ids = [all_names[1][i] for i in matched_name_indices]
+    #print(matched_ids)
     sparql_query =   """PREFIX jams:<http://w3id.org/polifonia/ontology/jams/>
                         PREFIX mm: <http://w3id.org/polifonia/ontology/music-meta/>
-                        SELECT DISTINCT ?tune_name ?tuneType ?key ?signature
+                        SELECT DISTINCT ?tune_name ?tuneType ?key ?signature ?id
                         {
                             ?tune mm:title ?tune_name.
+                            ?tune jams:tuneId ?id.
                             OPTIONAL {?tune jams:tuneType ?tuneType}
                             OPTIONAL {?tune jams:key ?key}
                             OPTIONAL {?tune jams:timeSignature ?signature}
-                            FILTER (?tune_name = \""""
-    sparql_query += "\" || ?tune_name = \"".join(matched_names)
-    sparql_query += "\").}"
-    return sparql_query
-
-
-# Old version
-def get_tune_given_name2(name):
-    sparql_query =   """PREFIX jams:<http://w3id.org/polifonia/ontology/jams/>
-                        PREFIX mm: <http://w3id.org/polifonia/ontology/music-meta/>
-                        SELECT DISTINCT ?tune_name ?tuneType ?key ?signature
-                        {
-                            ?tune mm:title ?tune_name.
-                            ?tune jams:tuneType ?tuneType.
-                            ?tune jams:key ?key.
-                            ?tune jams:timeSignature ?signature .
-                            FILTER (CONTAINS( ?tune_name, \"""" + name + """\" )).
-                        } LIMIT 10
-                     """
+                            VALUES (?id) { ( \""""
+    sparql_query += "\" ) ( \"".join(matched_ids)
+    sparql_query += "\" ) }\n}"
+    #print(sparql_query)
     return sparql_query
 
 
 # Advanced search
 def advanced_search(query_params, all_names):
-    if query_params['composition']:
-        matched_tuples = process.extractBests(query_params['composition'], all_names, score_cutoff=60, limit=100)
-        #print(matched_tuples)
-        matched_names = [x[0] for x in matched_tuples]
+    if query_params['title']:
+        names_dict = {i: val for i, val in enumerate(all_names[0])}
+        matched_tuples = process.extractBests(query_params['title'], names_dict,
+                                              score_cutoff=60)
+        matched_name_indices = [x[2] for x in matched_tuples]
+        matched_ids = [all_names[1][i] for i in matched_name_indices]
         title_query = True
     else:
         title_query = False
@@ -86,32 +83,126 @@ def advanced_search(query_params, all_names):
     sparql_query =   """PREFIX jams:<http://w3id.org/polifonia/ontology/jams/>
                         PREFIX mm:<http://w3id.org/polifonia/ontology/music-meta/>
                         PREFIX ptn:<http://w3id.org/polifonia/resource/pattern/>
-                        SELECT distinct ?tune_name ?tuneType ?key ?signature
+                        PREFIX ttype:<http://w3id.org/polifonia/resource/tunetype>
+                        PREFIX key:<http://w3id.org/polifonia/resource/key>
+                        PREFIX tsig:<http://w3id.org/polifonia/resource/timeSignature>
+                        PREFIX prov:<http://www.w3.org/ns/prov#>
+                        SELECT distinct ?tune_name ?tuneType ?key ?signature ?id
                         WHERE
                         {
-	                        ?obs jams:ofPattern ptn:""" + query_params['pattern'] + """.
+                        """
+
+    if query_params['pattern']:
+        sparql_query +=     "?obs jams:ofPattern ptn:" + query_params['pattern'] + """.
                             ?annotation jams:includesObservation ?obs.
-                            ?annotation jams:isJAMSAnnotationOf ?musicalComposition.
-                            ?musicalComposition mm:title ?tune_name.
-                            OPTIONAL {?tune jams:tuneType ?tuneType}
-                            OPTIONAL {?tune jams:key ?key}
-                            OPTIONAL {?tune jams:timeSignature ?signature}"""
-    if title_query:
-        sparql_query +=  """FILTER (?tune_name = \""""
-        sparql_query += "\" || ?tune_name = \"".join(matched_names)
-        sparql_query += "\").}"
+                            ?annotation jams:isJAMSAnnotationOf ?tune.
+                            """
+
+    if query_params['corpus']:
+        sparql_query +=     """?file prov:wasDerivedFrom ?corpus .
+                            FILTER (CONTAINS(?corpus, \""""+ query_params['corpus'] + """\")) .
+                            ?tune prov:wasDerivedFrom ?file.
+                            """
+
+    if query_params['tuneType']:
+        sparql_query +=     "?tune jams:tuneType ttype:" + query_params['tuneType'] + " .\n"
     else:
-        sparql_query += "}"
+        sparql_query +=     "OPTIONAL {?tune jams:tuneType ?tuneType}\n"
+
+    if query_params['key']:
+        sparql_query +=     "?tune jams:key key:" + query_params['key'] + " .\n"
+    else:
+        sparql_query +=     "OPTIONAL {?tune jams:key ?key}\n"
+
+    if query_params['timeSignature']:
+        sparql_query += "   ?tune jams:timeSignature tsig:" + query_params['timeSignature'] + " .\n"
+    else:
+        sparql_query += "   OPTIONAL {?tune jams:timeSignature ?signature}\n"
+
+    if title_query:
+        sparql_query +=  """?tune mm:title ?tune_name .
+                            VALUES (?id) { ( \""""
+        sparql_query += "\" ) ( \"".join(matched_ids)
+        sparql_query += "\" ) }\n"
+    else:
+        sparql_query += "   OPTIONAL {?tune mm:title ?tune_name}\n"
+
+    sparql_query +=     "?tune jams:tuneId ?id.}"
     return sparql_query
 
 
 def get_all_tune_names():
     sparql_query =   """PREFIX mm: <http://w3id.org/polifonia/ontology/music-meta/>
-                        SELECT DISTINCT ?tune_name
+                        PREFIX jams:<http://w3id.org/polifonia/ontology/jams/>
+                        SELECT DISTINCT ?title ?id
                         {
-                            ?tune_file mm:title ?tune_name.
+                            ?tune mm:title ?title.
+                            ?tune jams:tuneId ?id.
                         }
                      """
+    return sparql_query
+
+
+# Get the pattern node data for the network visualisation.
+def get_neighbour_patterns_by_tune(id, excludeTrivialPatterns):
+    sparql_query =   """PREFIX jams:<http://w3id.org/polifonia/ontology/jams/>
+                        PREFIX mm: <http://w3id.org/polifonia/ontology/music-meta/>
+                        SELECT ?pattern (count(?pattern) as ?patternFreq)
+                        {
+                            ?tune jams:tuneId \"""" + id + """\".
+                            ?xx jams:isJAMSAnnotationOf ?tune.
+                            ?xx jams:includesObservation ?observation .
+                        """
+    if excludeTrivialPatterns == "true":
+        sparql_query += """ ?observation jams:hasPatternComplexity ?comp .
+                            FILTER (?comp > "0.4"^^xsd:float) .
+                        """
+    sparql_query +=     """?observation jams:ofPattern ?pattern .
+                        } group by ?pattern order by DESC (?patternFreq) LIMIT 5"""
+    return sparql_query
+
+
+# Get the tune node data for the network visualisation.
+def get_neighbour_tunes_by_pattern(pattern):
+    sparql_query =       """PREFIX jams:<http://w3id.org/polifonia/ontology/jams/>
+                            PREFIX mm:<http://w3id.org/polifonia/ontology/music-meta/>
+                            PREFIX ptn:<http://w3id.org/polifonia/resource/pattern/>
+                            SELECT distinct ?title ?id
+                            WHERE
+                            {
+                                ?obs jams:ofPattern ptn:""" + pattern + """.
+                                ?annotation jams:includesObservation ?obs.
+                                ?annotation jams:isJAMSAnnotationOf ?musicalComposition.
+                                ?musicalComposition jams:tuneId ?id.
+                                OPTIONAL {?musicalComposition mm:title ?title}
+                            } LIMIT 5"""
+    return sparql_query
+
+
+def get_tune_data(id):
+    sparql_query =   """PREFIX jams:<http://w3id.org/polifonia/ontology/jams/>
+                        PREFIX mm:<http://w3id.org/polifonia/ontology/music-meta/>
+                        SELECT ?title ?tuneFamily
+                        {
+                            ?tune jams:tuneId \"""" + id + """\".
+                            ?tune mm:title ?title.
+                            ?tune jams:tuneFamily ?tuneFamily.
+                        }"""
+    return sparql_query
+
+
+def get_tune_family_members(family):
+    sparql_query =       """PREFIX jams:<http://w3id.org/polifonia/ontology/jams/>
+                            PREFIX mm:<http://w3id.org/polifonia/ontology/music-meta/>
+                            PREFIX fam:<http://w3id.org/polifonia/resource/tunefamily/>
+                            SELECT distinct ?title ?id ?type
+                            WHERE
+                            {
+                                ?tune jams:tuneFamily fam:"""+family+""".
+                                OPTIONAL {?tune mm:title ?title}
+                                OPTIONAL {?tune jams:tuneId ?id}
+                                OPTIONAL {?tune jams:tuneType ?type}
+                            } ORDER BY ASC (?title)"""
     return sparql_query
 
 
